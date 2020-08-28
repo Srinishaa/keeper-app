bee8ddd3bbc7b008af265fca356544e71c691100
Srinishaa <srinishaa@gmail.com>
2020-08-28 22:19:54 +0530

    keeper app

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AssistantIcon from "@material-ui/icons/Assistant";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <AssistantIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
